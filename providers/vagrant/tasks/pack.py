5e21c7bc1a62fbf5aab8e8a6418dd22f570df305
Olivier Sallou <olivier.sallou@irisa.fr>
2013-09-08 09:05:47 +0200

    add vagrant provider

---
+++ providers/vagrant/tasks/pack.py
@@ -0,0 +1,32 @@
+from base import Task
+from common import phases
+from common.tasks import packages
+from common.tasks.host import CheckPackages
+import os
+
+class CreateBox(Task):
+	description = 'Create Vagrant Box from image'
+	phase = phases.image_registration
+
+	def run(self, info):
+        print "NOT YET IMPLEMENTED"
+
+class VagrantConfig(Task):
+    description = 'Customize image for Vagrant'
+    phase = phases.system_modification
+
+    def run(self, info):
+        from shutil import copy
+        assets_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), 'assets'))
+        ssh_pub = os.path.join(assets_dir, 'ssh/vagrant.pub')
+        os.chmod(script_dst, rwxr_xr_x)
+
+        from common.tools import log_check_call
+        log_check_call(['/bin/mkdir', '-p',
+                        os.path.join(info.root,'root/.ssh')])
+        authorized_keys = os.path.join(info.root,
+                            'root/.ssh/authorized_keys')
+        copy(ssh_pub, authorized_keys)
+        log_check_call(['/usr/sbin/chroot', info.root,
+                        '/bin/chmod', '600', '/root/.ssh/authorized_keys'])
+
